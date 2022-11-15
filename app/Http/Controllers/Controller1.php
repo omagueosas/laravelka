13cef197a2daabf35dd136113a61fc43da93b668
omagueosas <saskakuzaw@gmail.com>
2022-11-15 03:19:16 -0800

    three commit

---
+++ app/Http/Controllers/Controller1.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Http\Controllers; 
+use App\Http\Controllers\Controller; 
+class Controller1 extends Controller 
+{ 
+	public function page()
+	{
+		return view('first');
+	}
+}
--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -8,20 +8,35 @@
 </head>
 <body>
     <header>
+        <div class="logo"></div>
         <div class="header-menu"><a href="">О нас</a></div>
-        <div class="header-menu"><a href="">Каталог</a></div>
+        <div class="header-menu"><a href="pexxedrg/catalog">Каталог</a></div>
         <div class="header-menu"><a href="">Где нас найти?</a></div>
 
         <div class="header-login"><a href="">Регистрация</a></div>
         <div class="header-login"><a href="">Вход</a></div>
     </header>
 
-    <div class="category">Каталог товаров</div>
+    <div class="category"><h2>Каталог товаров</h2></div>
     <div class="catalog">
-        <div class="menu-streamlining">Год производства Наименование Цена</div>
-        <div class="menu-category">Marvel Dc Other</div>
 
-        <div class=""></div>
+        <div class="menu-streamlining">
+            <div class="menu-streamlining-proizv">
+                Год производства Наименование Цена
+            </div>
+            <div class="menu-category">Marvel Dc Other</div>
+        </div>
+
+        <div class="product">
+            <div class="image"></div>
+            <div class="name"><p>Марвел</p></div>
+            <div class="price"><p>1000р</p></div>
+            <div class="go"><a href="">Перейти на товар</a></div>
+        </div>
+    </div>
+
+    <div class="basament">
+        <p>подвал</p>
     </div>
 </body>
 </html>
