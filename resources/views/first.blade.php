1f0658887adfb051e1c8689fe2badffdd632cb54
omagueosas <saskakuzaw@gmail.com>
2022-11-15 03:00:29 -0800

    first1 commit

---
+++ resources/views/first.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header-menu"><a href="">О нас</a></div>
+        <div class="header-menu"><a href="">Каталог</a></div>
+        <div class="header-menu"><a href="">Где нас найти?</a></div>
+
+        <div class="header-login"><a href="">Регистрация</a></div>
+        <div class="header-login"><a href="">Вход</a></div>
+    </header>
+
+    <div class="category">Каталог товаров</div>
+    <div class="catalog">
+        <div class="menu-streamlining">Год производства Наименование Цена</div>
+        <div class="menu-category">Marvel Dc Other</div>
+
+        <div class=""></div>
+    </div>
+</body>
+</html>
