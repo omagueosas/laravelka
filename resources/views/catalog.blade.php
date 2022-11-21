8d6644fae63967ab321aca6a97c49d22f3bea5dc
omagueosas <saskakuzaw@gmail.com>
2022-11-21 00:58:39 -0800

    21.11

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -4,68 +4,76 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/larapraktika/public/css/style.css">
     <title>Document</title>
 </head>
 <body>
     <header>
         <ul class="ul-menu">
             <li class="logo"><img src="" alt=""/></li>
-            <li class=""><a href="">О нас</a></li>
-            <li class=""><a href="">Каталог</a></li>
-            <li class=""><a href="">Где нас найти?</a></li>
-            <li class=""><a href="">Регистрация</a></li>
-            <li class=""><a href="">Вход</a></li>
+            <li><a href="">О нас</a></li>
+            <li><a href="">Каталог</a></li>
+            <li><a href="">Где нас найти?</a></li>
+            <li><a href="">Регистрация</a></li>
+            <li><a href="">Вход</a></li>
         </ul>
     </header>
     <div class="main-category">
-        <div class="category"><h2>Каталог товаров</h2></div>
-            <div class="catalog">
-                <div class="menu-streamlining">
-                    <div class="menu-streamlining-proizv">
-                        <ul>
-                            <li>Год производства</li>
-                            <li>Наименование</li>
-                            <li>Цена</li>
-                        </ul>
-                    </div>
-                    <div class="menu-category">
-                        <ul>
-                            <li>Marvel</li>
-                            <li>Dc</li>
-                            <li>Other</li>
-                        </ul>
-                    </div>
+        <div class="catalog">
+            <div class="menu-streamlining">
+                <div class="menu-streamlining-feature">
+                    <ul>
+                        <li>Год производства</li>
+                        <li>Наименование</li>
+                        <li>Цена</li>
+                    </ul>
+                </div>
+                <div class="menu-category">
+                    <ul>
+                        <li>Marvel</li>
+                        <li>Dc</li>
+                        <li>Other</li>
+                    </ul>
                 </div>
             </div>
+        </div>
+
+        <div class="category-name"><h2>Каталог товаров</h2></div>
 
         <div class="product-category">
             <div class="product">
                 <div class="image"><img src="" alt=""/></div>
-                <div class="name"><p>Марвел</p></div>
+                <div class="name"><p>Человек паук</p></div>
+                <div class="publisher">Издатель: Марвел</div>
+                <div class="price"><p>1000р</p></div>
+                <div class="go"><a href="">Перейти на товар</a></div>
+            </div>
+            <div class="product">
+                <div class="image"><img src="" alt=""/></div>
+                <div class="name"><p>Человек паук</p></div>
+                <div class="publisher">Издатель: Марвел</div>
+                <div class="price"><p>1000р</p></div>
+                <div class="go"><a href="">Перейти на товар</a></div>
+            </div>
+            <div class="product">
+                <div class="image"><img src="" alt=""/></div>
+                <div class="name"><p>Человек паук</p></div>
                 <div class="publisher">Марвел</div>
                 <div class="price"><p>1000р</p></div>
                 <div class="go"><a href="">Перейти на товар</a></div>
             </div>
             <div class="product">
                 <div class="image"><img src="" alt=""/></div>
-                <div class="name"><p>Марвел</p></div>
-                <div class="publisher">Марвел</div>
-                <div class="price"><p>1000р</p></div>
-                <div class="go"><a href="">Перейти на товар</a></div>
-            </div>
-            <div class="product">
-                <div class="image"><img src="" alt=""/></div>
-                <div class="name"><p>Марвел</p></div>
-                <div class="publisher">Марвел</div>
+                <div class="name"><p>Человек паук</p></div>
+                <div class="publisher">Издатель: Марвел</div>
                 <div class="price"><p>1000р</p></div>
                 <div class="go"><a href="">Перейти на товар</a></div>
             </div>
         </div>
     </div>
 
-    <div class="basament">
-            <p>подвал</p>
+    <div class="footer">
+            <p>footer</p>
     </div>  
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('pexxedrg/catalog', [App\Http\Controllers\Controller1::class, 'page']); 
 
-Route::get('pexxedrg/admin', [App\Http\Controllers\Controller1::class, 'page']); 
+//Route::get('pexxedrg/admin', [App\Http\Controllers\Controller1::class, '']); 
