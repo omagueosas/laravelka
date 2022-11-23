47c2a6e95e92b6553a30715727fdc4518065c69e
omagueosas <saskakuzaw@gmail.com>
2022-11-23 01:09:12 -0800

    23.11 v3.1

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -11,30 +11,35 @@
     <header>
         <ul class="ul-menu">
             <li class="logo"><img src="" alt=""/></li>
-            <li><a href="">О нас</a></li>
             <li><a href="">Каталог</a></li>
+            <li><a href="">О нас</a></li>
             <li><a href="">Где нас найти?</a></li>
             <li><a href="">Регистрация</a></li>
             <li><a href="">Вход</a></li>
         </ul>
+        <div class="header-info">
+            <img>
+            <p>Интернет магазин по всей России</p>
+            <p>Наш номер телефона: 8-902-679-14-39</p>
+            <p>Наша почта: saskakuzaw@gmail.com</p>
+            <p></p>
+        </div>
     </header>
     <div class="main-category">
         <div class="catalog">
             <div class="menu-streamlining">
-                <div class="menu-streamlining-feature">
                     <ul>
                         <li>Год производства</li>
                         <li>Наименование</li>
                         <li>Цена</li>
                     </ul>
-                </div>
-                <div class="menu-category">
-                    <ul>
-                        <li>Marvel</li>
-                        <li>Dc</li>
-                        <li>Other</li>
-                    </ul>
-                </div>
+            </div>
+            <div class="menu-category">
+                <ul>
+                    <li>Marvel</li>
+                    <li>Dc</li>
+                    <li>Other</li>
+                </ul>
             </div>
         </div>
 
@@ -42,38 +47,44 @@
 
         <div class="product-category">
             <div class="product">
-                <div class="image"><img src="" alt=""/></div>
-                <div class="name"><p>Человек паук</p></div>
-                <div class="publisher">Издатель: Марвел</div>
-                <div class="price"><p>1000р</p></div>
-                <div class="go"><a href="">Перейти на товар</a></div>
+                <div class="product-image"><img src="" alt=""/></div>
+                <div class="product-name"><p>Человек паук</p></div>
+                <div class="product-publisher">Марвел</div>
+                <div class="product-price"><p>1000р</p></div>
+                <div class="product-go"><a href="">Перейти на товар</a></div>
             </div>
             <div class="product">
-                <div class="image"><img src="" alt=""/></div>
-                <div class="name"><p>Человек паук</p></div>
-                <div class="publisher">Издатель: Марвел</div>
-                <div class="price"><p>1000р</p></div>
-                <div class="go"><a href="">Перейти на товар</a></div>
+                <div class="product-image"><img src="" alt=""/></div>
+                <div class="product-name"><p>Человек паук</p></div>
+                <div class="product-publisher">Марвел</div>
+                <div class="product-price"><p>1000р</p></div>
+                <div class="product-go"><a href="">Перейти на товар</a></div>
             </div>
             <div class="product">
-                <div class="image"><img src="" alt=""/></div>
-                <div class="name"><p>Человек паук</p></div>
-                <div class="publisher">Марвел</div>
-                <div class="price"><p>1000р</p></div>
-                <div class="go"><a href="">Перейти на товар</a></div>
+                <div class="product-image"><img src="" alt=""/></div>
+                <div class="product-name"><p>Человек паук</p></div>
+                <div class="product-publisher">Марвел</div>
+                <div class="product-price"><p>1000р</p></div>
+                <div class="product-go"><a href="">Перейти на товар</a></div>
             </div>
             <div class="product">
-                <div class="image"><img src="" alt=""/></div>
-                <div class="name"><p>Человек паук</p></div>
-                <div class="publisher">Издатель: Марвел</div>
-                <div class="price"><p>1000р</p></div>
-                <div class="go"><a href="">Перейти на товар</a></div>
+                <div class="product-image"><img src="" alt=""/></div>
+                <div class="product-name"><p>Человек паук</p></div>
+                <div class="product-publisher">Марвел</div>
+                <div class="product-price"><p>1000р</p></div>
+                <div class="product-go"><a href="">Перейти на товар</a></div>
             </div>
         </div>
     </div>
 
     <div class="footer">
             <p>footer</p>
+        <div class="footer-menu">
+
+        </div>
+        <div class="footer-info">
+
+        </div>
     </div>  
 </body>
 </html>
